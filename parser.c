133862556530c2388c1c92628b8a7bf88aa93c93
Cassandra Hall <mckay.hall@yahoo.com>
2025-11-26 11:54:52 -0700

    basic token type assignment, needs cleanup

--- parser.c
+++ parser.c
@@ -1,7 +1,76 @@
 #pragma once
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "structs.h"
+
+int typereturn(char *input){
+	int i;
+	char name[20];
+	char c;
+	for (i = 0; i < 20; i++){
+		name[i] = '\0';
+	}
+	for (i = 0; i < 20; i++){
+		c = input[i];
+		if (c != '\0' && c != ' '){
+			c = toupper(c);
+			name[i] = c;
+		}
+	}
+	if (!strcmp(name,"CREATE")){
+		return CREATE;
+	}
+	if (!strcmp(name,"DROP")){
+		return DROP;
+	}
+	if (!strcmp(name,"ALTER")){
+		return ALTER;
+	}
+	if (!strcmp(name,"DB")){
+		return DB;
+	}
+	if (!strcmp(name,"TABLE")){
+		return TABLE;
+	}
+	if (!strcmp(name,"ROW")){
+		return ROW;
+	}
+	if (!strcmp(name,"COLUMN")){
+		return COLUMN;
+	}
+	if (!strcmp(name,"ADD")){
+		return ADD;
+	}
+	if (!strcmp(name,"SELECT")){
+		return SELECT;
+	}
+	if (!strcmp(name,"UPDATE")){
+		return UPDATE;
+	}
+	if (!strcmp(name,"INSERT")){
+		return INSERT;
+	}
+	if (!strcmp(name,"DELETE")){
+		return DELETE;
+	}
+	if (!strcmp(name,"FROM")){
+		return FROM;
+	}
+	if (!strcmp(name,"WHERE")){
+		return WHERE;
+	}
+	if (!strcmp(name,"*")){
+		return ASTERISK;
+	}
+	if (!strcmp(name,";")){
+		return SEMICOLON;
+	}
+	if (!strcmp(name,"=")){
+		return EQUALS;
+	}
+	return GENERIC;
+}
 
 void scanner(tokenlist *tokenlist, char *string){
 	int i,j,k,l;
@@ -11,8 +80,11 @@ void scanner(tokenlist *tokenlist, char *string){
 	k = 0;
 	for (i = 0;i < 100;i++){
 		c = string[i];
-		printf("%c\n",c);
-		tokenbuffer[j] = c;
+		if (c == ';'){
+
+		} else {
+			tokenbuffer[j] = c;
+		}
 		if (c == '\0' || c == ';'){
 			i = 100;
 			sprintf(tokenlist->tokens[k].name,"%s",tokenbuffer);
@@ -30,63 +102,10 @@ void scanner(tokenlist *tokenlist, char *string){
 		}
 	}
 	for (l = 0; l <= k; l++){
-		printf("%s ",tokenlist->tokens[l].name);
+		tokenlist->tokens[l].type = typereturn(tokenlist->tokens[l].name);
+		printf("%i:%s ",tokenlist->tokens[l].type,tokenlist->tokens[l].name);
 	}
 	printf("\n");
-}
-
-int typereturn(token *token){
-	if (strcmp(token->name,"CREATE")){
-		return CREATE;
-	}
-	if (strcmp(token->name,"DROP")){
-		return DROP;
-	}
-	if (strcmp(token->name,"ALTER")){
-		return ALTER;
-	}
-	if (strcmp(token->name,"DB")){
-		return DB;
-	}
-	if (strcmp(token->name,"TABLE")){
-		return TABLE;
-	}
-	if (strcmp(token->name,"ROW")){
-		return ROW;
-	}
-	if (strcmp(token->name,"COLUMN")){
-		return COLUMN;
-	}
-	if (strcmp(token->name,"ADD")){
-		return ADD;
-	}
-	if (strcmp(token->name,"SELECT")){
-		return SELECT;
-	}
-	if (strcmp(token->name,"UPDATE")){
-		return UPDATE;
-	}
-	if (strcmp(token->name,"INSERT")){
-		return INSERT;
-	}
-	if (strcmp(token->name,"DELETE")){
-		return DELETE;
-	}
-	if (strcmp(token->name,"FROM")){
-		return FROM;
-	}
-	if (strcmp(token->name,"WHERE")){
-		return WHERE;
-	}
-	if (strcmp(token->name,"*")){
-		return ASTERISK;
-	}
-	if (strcmp(token->name,";")){
-		return SEMICOLON;
-	}
-	if (strcmp(token->name,"=")){
-		return EQUALS;
-	}
 }
 
 void lexer(tokenlist *tokenlist, char *string){
